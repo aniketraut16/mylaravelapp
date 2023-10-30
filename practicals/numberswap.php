41f8d81d28cfa450ea4ebf9aea957a2d5ec337f6
aniketraut16 <rautan_1@rknec.edu>
2023-10-30 18:33:50 +0530

    Added Number Swapping Code

---
+++ practicals/numberswap.php
@@ -0,0 +1,12 @@
+<?php
+
+$n1 = 2;
+$n2 = 3;
+echo "Before Swapping \n";
+echo "Number 1 = " , $n1 , " && Number 2 = ", $n2 ;
+$temp = $n1;
+$n1 = $n2;
+$n2 = $temp;
+echo "\nAfter Swapping \n";
+echo "Number 1 = " , $n1 , " && Number 2 = ", $n2 ;
+?>
