57c1eeb11618cb28c5315025ed8fc48139914f36
aniketraut16 <rautan_1@rknec.edu>
2023-10-31 18:30:13 +0530

    Updated fibonacci program  for readability and mantainability

--- practicals/fibonacci.php
+++ practicals/fibonacci.php
@@ -1,12 +1,37 @@
 <?php
-$n = 10;
-$n1 = 0;
-$n2 = 1;
-echo $n1," ",$n2," ";
-for ($i = 2; $i <= $n; $i++){
-$n3 = $n1 + $n2;
-echo $n3," ";
-$n1 = $n2;
-$n2 = $n3;
+// Function to find fibonacci number
+function fibonacci($n) {
+
+    // Base case
+
+    if ($n == 0 || $n == 1) {
+        return $n;
+    } else {
+
+        // Recursive case: calculate the Fibonacci number by summing the previous two Fibonacci numbers
+        
+        $fibonacci_minus_1 = fibonacci($n - 1);
+        $fibonacci_minus_2 = fibonacci($n - 2);
+        $fibonacci_number = $fibonacci_minus_1 + $fibonacci_minus_2;
+        return $fibonacci_number;
+
+    }
+}
+
+// Number of Fibonacci numbers to print
+$num_terms = (int) readline("Enter Number of terms to display in Fibonacci Sequence:");
+
+echo "Fibonacci Sequence for the first $num_terms terms:\n";
+
+// Print the Fibonacci Sequence
+for ($i = 0; $i < $num_terms; $i++) {
+    $fibonacci_number = fibonacci($i);
+    echo "$fibonacci_number";
+    
+    if ($i < $num_terms - 1) {
+        echo ", ";
+    } else {
+        echo "\n";
+    }
 }
 ?>
