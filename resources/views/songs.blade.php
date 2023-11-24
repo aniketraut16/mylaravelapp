a5239ac1386c2c084ed65c14cfbf2953d8a8d0e2
aniketraut16 <rautan_1@rknec.edu>
2023-11-24 20:52:58 +0530

    Created a component

--- resources/views/songs.blade.php
+++ resources/views/songs.blade.php
@@ -1,24 +1,2 @@
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-    <table>
-<tr>
-<th>Title</th>
-<th>Artist</th>
-</tr>
-@foreach ($songs as $song)
-<tr>
-<td>{{ $song->getTitle() }}</td>
-<td>{{ $song->getArtist() }}</td>
-</tr>
-@endforeach
-</table>
-    
-</body>
-</html>
+<x-layouts.main>
+</x-layouts.main>
